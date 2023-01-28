49b473880ec598e7f94ba52345f47f4caa64c2ad
DzunN86 <nurroinz@gmail.com>
2023-01-28 22:15:27 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,13 @@ import MenuPage from "./Menu";
 import MenuDetailPage from "./MenuDetail";
 import ScanPage from "./Scan";
 import CheckoutPage from "./Checkout";
+import Confirm from "./Confirm";
 
-export { RestaurantsPage, MenuPage, MenuDetailPage, ScanPage, CheckoutPage };
+export {
+  RestaurantsPage,
+  MenuPage,
+  MenuDetailPage,
+  ScanPage,
+  CheckoutPage,
+  Confirm,
+};
