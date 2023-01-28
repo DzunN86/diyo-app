6d0dd71735ee63c6314fbf30a2d6b2b8ebf0b4cf
DzunN86 <nurroinz@gmail.com>
2023-01-28 12:48:45 +0700

    click 1 product go to scan page

--- src/pages/Restaurants.jsx
+++ src/pages/Restaurants.jsx
@@ -14,7 +14,7 @@ function RestaurantsPage() {
             {dataRestaurants.map((restaurant) => (
               <CardProduct
                 key={restaurant.id}
-                to={`/${restaurant.slug}`}
+                to={`/${restaurant.slug}/scan`}
                 name={restaurant.name}
                 image={restaurant.image}
                 address={restaurant.address}
--- src/pages/Scan.jsx
+++ src/pages/Scan.jsx
@@ -1,10 +1,28 @@
+import { QrScanner } from "@yudiel/react-qr-scanner";
 import React from "react";
 
 function ScanPage() {
   return (
-    <div>
-      <h1>ScanPage</h1>
-    </div>
+    <>
+      <div className="fixed top-0 z-30 w-full max-w-screen-xs bg-transparent">
+        <div className="flex items-center text-white gap-4 py-3 px-4 text-xl font-bold">
+          <button className="flex bg-primary px-1 rounded-full">
+            <i className="ri-arrow-left-line"></i>
+          </button>
+        </div>
+      </div>
+      <div className="bg-black min-h-screen flex items-center">
+        <div className="w-full">
+          <QrScanner
+            onDecode={(result) => console.log(result)}
+            onError={(error) => console.log(error?.message)}
+          />
+          <div className="flex justify-center py-3 px-4">
+            <p className="text-primary">Silahkan scan barcode di meja!</p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
