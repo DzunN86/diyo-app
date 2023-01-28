1fcabbe389942e7b4e2624a7a67822815432103c
DzunN86 <nurroinz@gmail.com>
2023-01-28 22:49:40 +0700

    Update Confirm.jsx

--- src/pages/Confirm.jsx
+++ src/pages/Confirm.jsx
@@ -9,8 +9,8 @@ function Confirm() {
       <img src={IlusConfirm} alt="ilustrasi" className="mb-4" />
       <div className="flex flex-col gap-2 items-center">
         <h1 className="text-3xl font-semibold">Order Confirmed!</h1>
-        <p className="text-gray-500">
-          Silahkan tunggu pesanan anda akan segera kami sajikan
+        <p className="text-gray-500 text-sm w-72 text-center">
+          Silahkan tunggu pesanan anda, pesanan akan segera diantar
         </p>
         <button
           onClick={() => navigate("/")}
