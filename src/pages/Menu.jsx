134b8ddbfc85a552d2d1dcb0ecbfccaeb24ddbe0
DzunN86 <nurroinz@gmail.com>
2023-01-28 22:15:52 +0700

    integrate data menu

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,22 +1,25 @@
-import React, { useState } from "react";
-import { useParams } from "react-router-dom";
-import { BottomAction, CardProduct } from "../components";
+import React, { useContext, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { BottomAction, CardProduct, NavBack } from "../components";
 import { dataRestaurants } from "../constans";
+import { AppContext } from "../context";
 import { idrFormatter } from "../utils";
 
 function MenuPage() {
   const [catalog, setCatalog] = useState(0);
-  const { slug } = useParams();
+  const [like, setLike] = useState(false);
+
+  // get context
+  const { basket, setBasket } = useContext(AppContext);
+
+  const { slug, no_meja } = useParams();
+  const navigate = useNavigate();
+
   const restaurant = dataRestaurants.find((item) => item.slug === slug);
+
   return (
     <>
-      <div className="fixed top-0 z-30 w-full max-w-screen-xs bg-transparent">
-        <div className="flex items-center text-white gap-4 py-3 px-4 text-xl font-bold">
-          <button className="flex bg-primary px-1 rounded-full">
-            <i className="ri-arrow-left-line"></i>
-          </button>
-        </div>
-      </div>
+      <NavBack backTo="/" />
       <div className="h-56 bg-black relative">
         <img
           src={restaurant.image}
@@ -26,13 +29,20 @@ function MenuPage() {
         <div className="w-full h-full absolute bg-gradient-to-b from-black/40 via-black/0 to-black/40 inset-0"></div>
         <div className="flex bg-primary gap-2 absolute bottom-4 text-white px-2 py-[2px] text-sm rounded-md mx-4">
           <i className="ri-file-list-3-line"></i>
-          <p>No. Meja 1</p>
+          <p>No. Meja {no_meja}</p>
         </div>
       </div>
       <div className="bg-white px-4 pb-5">
-        <div className="bg-white flex justify-center items-center shadow-s2 w-12 h-12 rounded-full float-right -translate-y-1/2 -mb-5">
-          <i className="ri-heart-line text-3xl text-primary"></i>
-        </div>
+        <button
+          className="bg-white flex justify-center items-center shadow-s2 w-12 h-12 rounded-full float-right -translate-y-1/2 -mb-5"
+          onClick={() => setLike(!like)}
+        >
+          <i
+            className={`text-3xl text-primary ${
+              !like ? "ri-heart-line" : "ri-heart-fill"
+            }`}
+          ></i>
+        </button>
         <div className=" flex w-full flex-col gap-4">
           <div className="flex flex-col gap-1">
             <h1 className="text-3xl font-bold">Dominico Coffe</h1>
@@ -53,7 +63,7 @@ function MenuPage() {
           </div>
         </div>
       </div>
-      <div className="bg-white px-4 mt-3 pt-6">
+      <div className="bg-white px-4 mt-3 pt-6 pb-20">
         <div className="flex gap-2 overflow-auto">
           {restaurant.catalog.map((item, index) => (
             <button
@@ -67,9 +77,15 @@ function MenuPage() {
             </button>
           ))}
         </div>
-        <div className="flex flex-col mt-2 divide-y-2 w-full">
+        <div className="flex flex-col mt-2 divide-y w-full">
           {restaurant.catalog[catalog].menu.map((menu) => (
-            <div key={menu.id} className="flex py-6 gap-2 items-start w-full">
+            <button
+              key={menu.id}
+              onClick={() =>
+                navigate(`/${slug}/${no_meja}/menu/${catalog}/${menu.id}`)
+              }
+              className="flex py-6 gap-2 items-start w-full"
+            >
               <div className="w-1/3 h-20">
                 <img
                   src={menu.image}
@@ -83,11 +99,27 @@ function MenuPage() {
                   {idrFormatter(menu.price)}
                 </p>
               </div>
-            </div>
+            </button>
           ))}
         </div>
       </div>
-      <BottomAction quantity={0} link="View Basket" totalPrice="Rp 5000" />
+      {basket.length > 0 && (
+        <BottomAction
+          quantity={
+            basket.reduce((total, item) => total + item.quantity, 0) || 0
+          }
+          link="View Basket"
+          onClick={() => navigate(`/${slug}/${no_meja}/checkout`)}
+          totalPrice={
+            idrFormatter(
+              basket.reduce(
+                (total, item) => total + item.quantity * item.price,
+                0
+              )
+            ) || 0
+          }
+        />
+      )}
     </>
   );
 }
