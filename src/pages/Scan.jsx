c1266536647414629434141bd2fca47ff907f80c
DzunN86 <nurroinz@gmail.com>
2023-01-28 22:16:18 +0700

    integrate Scan

--- src/pages/Scan.jsx
+++ src/pages/Scan.jsx
@@ -1,22 +1,37 @@
 import { QrScanner } from "@yudiel/react-qr-scanner";
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { NavBack } from "../components";
 
 function ScanPage() {
+  const [isScanning, setIsScanning] = React.useState(false);
+  const navigate = useNavigate();
+
+  const decodeQr = (result) => {
+    let splitResult = result.split(":");
+    navigate("/" + splitResult[0] + "/" + splitResult[1]);
+  };
+
+  useEffect(() => {
+    // open camera when mount
+    setIsScanning(true);
+    return () => {
+      // close camera when unmount
+      setIsScanning(false);
+    };
+  }, []);
   return (
     <>
-      <div className="fixed top-0 z-30 w-full max-w-screen-xs bg-transparent">
-        <div className="flex items-center text-white gap-4 py-3 px-4 text-xl font-bold">
-          <button className="flex bg-primary px-1 rounded-full">
-            <i className="ri-arrow-left-line"></i>
-          </button>
-        </div>
-      </div>
+      <NavBack />
       <div className="bg-black min-h-screen flex items-center">
         <div className="w-full">
-          <QrScanner
-            onDecode={(result) => console.log(result)}
-            onError={(error) => console.log(error?.message)}
-          />
+          {isScanning && (
+            <QrScanner
+              scanDelay={1000}
+              onDecode={decodeQr}
+              onError={(error) => console.log(error?.message)}
+            />
+          )}
           <div className="flex justify-center py-3 px-4">
             <p className="text-primary">Silahkan scan barcode di meja!</p>
           </div>
