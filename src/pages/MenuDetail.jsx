799fa613c79c00bfb22343c7ae2505d9bcfb12f1
DzunN86 <nurroinz@gmail.com>
2023-01-28 22:16:09 +0700

    integrate menu detail

--- src/pages/MenuDetail.jsx
+++ src/pages/MenuDetail.jsx
@@ -1,21 +1,48 @@
-import React from "react";
-import { BottomAction, Counter, TextArea } from "../components";
+import React, { useContext } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { BottomAction, Counter, NavBack, TextArea } from "../components";
+import { dataRestaurants } from "../constans";
+import { AppContext } from "../context";
+import { idrFormatter } from "../utils";
 
 function MenuDetailPage() {
   const [catatan, setCatatan] = React.useState("");
   const [quantity, setQuantity] = React.useState(1);
+
+  // get context
+  const { basket, setBasket } = useContext(AppContext);
+
+  const { slug, no_meja, catalog_id, menu_id } = useParams();
+  const navigate = useNavigate();
+
+  // find menu detail
+  const MenuDetail = dataRestaurants
+    .find((item) => item.slug === slug)
+    .catalog[catalog_id].menu.find((item) => item.id == menu_id);
+
+  // add to basket
+  const onAddToBasket = () => {
+    const data = {
+      ...MenuDetail,
+      quantity,
+      catatan,
+    };
+    try {
+      // add to basket
+      setBasket([...basket, data]);
+      // navigate to menu page
+      navigate(`/${slug}/${no_meja}`);
+    } catch (error) {
+      alert("Maaf, terjadi kesalahan");
+    }
+  };
+
   return (
     <>
-      <div className="fixed top-0 z-30 w-full max-w-screen-xs bg-transparent">
-        <div className="flex items-center text-white gap-4 py-3 px-4 text-xl font-bold">
-          <button className="flex bg-primary px-1 rounded-full">
-            <i className="ri-arrow-left-line"></i>
-          </button>
-        </div>
-      </div>
+      <NavBack />
       <div className="h-56 bg-black relative">
         <img
-          src="https://media-order.bkdelivery.co.id/thumb/product_photo/2022/12/19/dzc24j9bhxxzxjhmbvjkvj_product_list.jpg"
+          src={MenuDetail.image}
           alt="Photo"
           className="object-cover w-full h-full"
         />
@@ -23,8 +50,10 @@ function MenuDetailPage() {
       </div>
       <div className="px-4 mt-4">
         <div className="flex justify-between items-center border-b-2 border-primary/50 mb-4 pb-3">
-          <h1 className="text-2xl font-bold">Dominico Coffe</h1>
-          <p className="text-xl text-primary font-semibold">Rp 5000</p>
+          <h1 className="text-2xl font-bold">{MenuDetail.name}</h1>
+          <p className="text-xl text-primary font-semibold">
+            {idrFormatter(MenuDetail.price)}
+          </p>
         </div>
         <h1 className="text-lg font-semibold mb-2">Special Request</h1>
         <TextArea
@@ -43,8 +72,9 @@ function MenuDetailPage() {
       </div>
       <BottomAction
         quantity={quantity}
+        onClick={onAddToBasket}
         link="Add to Basket"
-        totalPrice="Rp 5000"
+        totalPrice={idrFormatter(MenuDetail.price * quantity)}
       />
     </>
   );
