d3db98704786adc62505604fa85105771e368827
DzunN86 <nurroinz@gmail.com>
2023-01-28 12:17:06 +0700

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,18 @@
 import CardProduct from "./CardProduct";
 import Header from "./Header";
 import BottomNav from "./BottomNav";
-export { CardProduct, Header, BottomNav };
+import TextArea from "./TextArea";
+import Counter from "./Counter";
+import Toggle from "./Toggle";
+import Input from "./Input";
+import BottomAction from "./BottomAction";
+export {
+  CardProduct,
+  Header,
+  BottomNav,
+  TextArea,
+  Counter,
+  Toggle,
+  Input,
+  BottomAction,
+};
